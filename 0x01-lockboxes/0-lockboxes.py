086c918b8955d94d4e115527b66f4f7b8d49c694
Rasheed Olaoluwa <radeyola@gmail.com>
2024-07-30 16:29:36 +0000

    remove whitespace

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -13,17 +13,17 @@ def canUnlockAll(boxes):
     """
     keys = {0: True}
     n_boxes = len(boxes)
-    
+
     while True:
         n_keys = len(keys)
-        
+
         for i in range(len(boxes)):
             if boxes[i] and keys.get(i, False):
                 for j in boxes[i]:
                     if j < n_boxes:
                         keys[j] = True
                 boxes[i] = None
-        
+
         if len(keys) == n_keys:
             break
 
